b3b5a010cbfea2d5f9436d3e9a8f6bf751783ef5
Dominic Sauder <dominic.sauder@gmail.com>
2018-04-24 07:52:21 +0200

    execute all statements in one transaction

--- SqlBatch/Program.cs
+++ SqlBatch/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Data;
 using System.Data.OleDb;
 using System.IO;
+using System.Linq;
 
 namespace SqlBatch
 {
@@ -15,34 +17,72 @@ namespace SqlBatch
             var connectionString = content.Substring(0, content.IndexOf(Environment.NewLine, StringComparison.Ordinal));
 
             // the following contents are statement, teminated with a semicolon
-            var statements = content.Substring(content.IndexOf(Environment.NewLine, StringComparison.Ordinal)).Trim().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            var statements = content.Substring(content.IndexOf(Environment.NewLine, StringComparison.Ordinal))
+                .Replace(Environment.NewLine, " ")
+                .Split(';')
+                .Select(s => s.Trim())
+                .Where(s => s.Length > 0);
 
             using (var connection = new OleDbConnection { ConnectionString = connectionString })
             {
                 Console.WriteLine("----------------------------------------------------");
                 Console.WriteLine($"Opening database with connection: {connectionString}");
                 connection.Open();
+                OleDbTransaction transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);
 
-                foreach (var statement in statements)
+                try
+                {
+                    foreach (var statement in statements)
+                    {
+                        Console.WriteLine("----------------------------------------------------");
+                        Console.WriteLine(statement);
+                        Console.WriteLine("----------------------------------------------------");
+                        var command = connection.CreateCommand();
+                        command.CommandText = statement + ";";
+                        command.Transaction = transaction;
+
+                        if (statement.ToLower().StartsWith("select"))
+                            ExecuteQuery(command);
+                        else
+                            ExecuteNonQuery(command);
+
+                    }
+                    Console.WriteLine("----------------------------------------------------");
+                    Console.WriteLine("Commit");
+                    Console.WriteLine("----------------------------------------------------");
+                    transaction.Commit();
+                }
+                catch (Exception ex)
                 {
                     Console.WriteLine("----------------------------------------------------");
-                    Console.WriteLine(statement);
+                    Console.WriteLine($"Error: {ex}");
+                    transaction.Rollback();
+                    Console.WriteLine("Rollback");
                     Console.WriteLine("----------------------------------------------------");
-                    var command = new OleDbCommand(statement + ";") {Connection = connection};
-                    var reader = command.ExecuteReader();
-                    var row = new object[reader.FieldCount];
-
-                    while (reader.Read())
-                    {
-                        int cols = reader.GetValues(row);
-                        for (var i = 0; i < cols; i++)
-                            Console.Write($"{row[i]}\t|");
-                        Console.WriteLine();
-                    }
                 }
             }
 
             Console.ReadKey();
+        }
+
+        private static void ExecuteNonQuery(IDbCommand command)
+        {
+            var affectedRows = command.ExecuteNonQuery();
+            Console.WriteLine($"Affected rows: {affectedRows}");
+        }
+
+        private static void ExecuteQuery(IDbCommand command)
+        {
+            var reader = command.ExecuteReader();
+            var row = new object[reader.FieldCount];
+
+            while (reader.Read())
+            {
+                int columnCount = reader.GetValues(row);
+                for (var i = 0; i < columnCount; i++)
+                    Console.Write($"{row[i]}\t|");
+                Console.WriteLine();
+            }
         }
     }
 }
