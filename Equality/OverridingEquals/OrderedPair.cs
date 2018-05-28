e4e1dcc1d2648ff058455570be74d2dc5fdf8058
Dominic Sauder <dominic.sauder@gmail.com>
2018-05-28 17:53:50 +0200

    added second equality exercise

---
+++ Equality/OverridingEquals/OrderedPair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OverridingEquals
+{
+    public class OrderedPair : IPair
+    {
+        public OrderedPair(int i, int i1)
+        {
+            First = i;
+            Second = i1;
+        }
+
+        public object First { get; set; }
+        public object Second { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (obj.GetType() != GetType()) return false;
+
+            return Equals((OrderedPair) obj);
+        }
+
+        private bool Equals(OrderedPair other)
+        {
+            return First.Equals(other.First) && Second.Equals(other.Second);
+        }
+
+        public override int GetHashCode()
+        {
+            const int hashSeed = 7;
+            const int hashMultiplier = 13;
+
+            var hash = hashSeed;
+            hash = (hash * hashMultiplier) ^ First.GetHashCode();
+            hash = (hash * hashMultiplier) ^ Second.GetHashCode();
+
+            return hash;
+        }
+    }
+}
---
+++ Equality/OverridingEquals/UnorderedPair.cs
@@ -0,0 +1,27 @@
+﻿namespace OverridingEquals
+{
+    public class UnorderedPair : IPair
+    {
+        public UnorderedPair(int i, int i1)
+        {
+            First = i;
+            Second = i1;
+        }
+
+        public object First { get; set; }
+        public object Second { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (obj.GetType() != GetType()) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return (First is null ? 0 : First.GetHashCode() ) ^ (Second is null ? 0 : Second.GetHashCode());}
+    }
+}
