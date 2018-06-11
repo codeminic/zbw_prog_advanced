1f4f73b84a9b30bd8613a4584eb7efd7be5a3c34
Dominic Sauder <dominic.sauder@solve.ch>
2018-06-11 16:16:15 +0200

    added wpf mvvm exercise

---
+++ WpfMvvm/MVVMExerciseVorgabe_SS2018/MVVMExercise/CustomersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using Datasource;
+using WpfExercise;
+using WpfExercise.Services;
+
+namespace MVVMExercise
+{
+    public class CustomersViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public Customer CurrentCustomer { get; set; }
+
+        public RelayCommand SaveCommand { get; set; }
+
+        public CustomersViewModel()
+        {
+            var customersRepository = new CustomersRepository();
+            Customers = new ObservableCollection<Customer>(customersRepository.GetCustomersAsync().Result);
+            SaveCommand = new RelayCommand(() => customersRepository.UpdateCustomerAsync(CurrentCustomer));
+        }
+    }
+}
