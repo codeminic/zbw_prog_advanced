1ad463cc354624a2ca3cb50f62a20791be718754
Dominic Sauder <dominic.sauder@solve.ch>
2018-06-12 14:03:43 +0200

    added exercise 1.4

--- Generics/1.4_TypeConstraints/Aufgabe3.cs
+++ Generics/1.4_TypeConstraints/Aufgabe3.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 namespace _1._4_TypeConstraints {
     // TODO: Versuchen Sie, folgende Methode nur mit Type Constraints kompilierfähig zu machen.
     //       Die Methode soll sämtliche Elemente von "source" in eine neue Liste abfüllen.
-    static class MyHelpers {
-        //static TDest CopyTo<TSource, TDest, TElement>(TSource source)
-        //{
-        //    TDest dest = new TDest();
-        //    foreach (TElement element in source)
-        //    {
-        //        dest.Add(element);
-        //    }
-        //    return dest;
-        //}
+    static class MyHelpers<TSource, TDest, TElement> where TDest : IList<TElement>, new()
+                                                    where TSource : IEnumerable<TElement>
+    {
+        static TDest CopyTo(TSource source)
+        {
+            TDest dest = new TDest();
+            foreach (TElement element in source)
+            {
+                dest.Add(element);
+            }
+            return dest;
+        }
     }
 }
